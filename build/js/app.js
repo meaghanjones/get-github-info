0634dbc9b853db821ebec1e1042db328cd5b8e2e
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-08-05 13:43:06 -0700

    change styling for repo name and description list and add a link to clone the repo

--- build/js/app.js
+++ build/js/app.js
@@ -12,15 +12,16 @@ User.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     for (var index = 0; index < response.length; index +=1){
       if (response[index].description !== null) {
-        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: " + response[index].description);
+        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: " + response[index].description + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
       } else if (response[index].description === " ") {
-        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given");
+        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given" + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
       } else {
-          $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given");
+          $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given" + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
       }
 
     };
     console.log(response.length)
+    console.log(response)
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
