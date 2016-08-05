464340a6617be5bf6678bdc94bfd3bb327434a10
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-08-05 14:41:34 -0700

    add ability to view the date the repos were created

--- js/user.js
+++ js/user.js
@@ -6,16 +6,14 @@ function User() {
 
 User.prototype.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName + '/repos?&per_page=300&access_token=' + apiKey).then(function(response){
-    // $("#showUserPhoto").text("<img src=" + response[0].owner.avatar_url) + ">");
     for (var index = 0; index < response.length; index +=1){
       if (response[index].description !== null) {
-        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: " + response[index].description + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
+        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: " + response[index].description + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>" + "<br>" + "Created at: " + moment(response[index].created_at).format("M-DD-YYYY"));
       } else if (response[index].description === " ") {
-        $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given" + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
+          $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given" + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>" + "<br>" + "Created at: " + moment(response[index].created_at).format("M-DD-YYYY"));
       } else {
-          $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given" + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
+          $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: none given" + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>" + "<br>" + "Created at: " + moment(response[index].created_at).format("M-DD-YYYY"));
       }
-
     }
     console.log(response.length);
     console.log(response);
