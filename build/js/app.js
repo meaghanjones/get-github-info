1cac7b1463f18c9715aae977c71f44064ccfa2f8
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-08-05 14:20:11 -0700

    add ability to view the first 300 repos of a github user

--- build/js/app.js
+++ build/js/app.js
@@ -9,7 +9,8 @@ function User() {
 }
 
 User.prototype.getRepos = function(userName){
-  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + userName + '/repos?&per_page=300&access_token=' + apiKey).then(function(response){
+    // $("#showUserPhoto").text("<img src=" + response[0].owner.avatar_url) + ">");
     for (var index = 0; index < response.length; index +=1){
       if (response[index].description !== null) {
         $("#showUserInfo").append("<li>" + "Repo Name: " + response[index].name + " || " + "Description: " + response[index].description + "<br>" + "Clone Repo here: " + "<a href = '" + response[index].clone_url + "'" + ">" + response[index].clone_url + "</a>");
