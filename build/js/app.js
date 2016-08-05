7c57f18c1cfab1ab542af678a79ef78c3a6c666d
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-08-05 11:42:57 -0700

    add a git hub octocat that is shown when the information is texted to the html

--- build/js/app.js
+++ build/js/app.js
@@ -24,13 +24,16 @@ exports.userModule = User;
 },{"./../.env":1}],3:[function(require,module,exports){
 var User = require('./../js/user.js').userModule;
 
+
 $(document).ready(function() {
+  $('#github-cat').hide();
   $('#newSearchUser').click(function(event){
     event.preventDefault();
     $('#showUserInfo').text("");
     var userName = $('#userName').val();
     newUserObject = new User();
     newUserObject.getRepos(userName);
+    $('#github-cat').show();
   });
 });
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,11 +1,14 @@
 var User = require('./../js/user.js').userModule;
 
+
 $(document).ready(function() {
+  $('#github-cat').hide();
   $('#newSearchUser').click(function(event){
     event.preventDefault();
     $('#showUserInfo').text("");
     var userName = $('#userName').val();
     newUserObject = new User();
     newUserObject.getRepos(userName);
+    $('#github-cat').show();
   });
 });
